015fa7ccb959fe4998d96992fafebe20ccea8201
Chance <chance3009@outlook.com>
2024-02-23 05:49:00 +0800

    Add another chart

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# House-price-prediction-using-flask
+# Social Media Sentiment Analysis
 Random Text
 
 ## Installation
 
-To run the web app on your local computer, install the required libraries. These packages are include in the requirement.txt file. This project used Python 3.8.0 and Flask 2.0.1.<br><br> Run the following command in the terminal to install the required packages.<br><br>
+To run the web app on your local computer, install the required libraries. These packages are include in the requirement.txt file. Run the following command in the terminal to install the required packages.<br><br>
 
 ```
 pip install -r requirement.txt
@@ -13,13 +13,12 @@ pip install -r requirement.txt
 
 ## Getting Started
 
-To run code on your computer, following command in terminal<br><br>
+To run the code on your computer, , use the following command in the terminal:<br><br>
 ```
 python app.py
 ```
 <br>
 <br>
-
-## Preview
+Visit [http://localhost:5000](Visit http://localhost:5000) in your web browser to access the sentiment analysis dashboard.
 
 ### Thank you
--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 sentiment_classifier = pipeline(
     model="lxyuan/distilbert-base-multilingual-cased-sentiments-student")
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,25 +15,42 @@ def home():
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    sentiment_counts = {'Positive': 0, 'Negative': 0, 'Neutral': 0}
+    sentiment_counts = {'positive': 0, 'negative': 0, 'neutral': 0}
+    country_distribution = {}
+
     hashtag = request.form['hashtag']
 
     df = pd.read_csv('social_media_data.csv', encoding='latin1')
 
+    # Filter post with specific hashtag input by user
     filtered_df = df[df['Hashtags'].str.contains(hashtag, case=False)]
 
     data = filtered_df['Text'].tolist()
-
+    countries = filtered_df['Country'].tolist()
     for i in range(len(data)):
         result = sentiment_classifier(data[i])[0]['label']
         if result == 'positive':
-            sentiment_counts['Positive'] += 1
+            sentiment_counts['positive'] += 1
         elif result == 'negative':
-            sentiment_counts['Negative'] += 1
+            sentiment_counts['negative'] += 1
         else:
-            sentiment_counts['Neutral'] += 1
+            sentiment_counts['neutral'] += 1
 
-    return render_template('dashboard.html', hashtag=hashtag, sentiment_counts=sentiment_counts)
+        # Update country_distribution
+        country = countries[i]
+        if country not in country_distribution:
+            country_distribution[country] = {
+                'positive': 0, 'negative': 0, 'neutral': 0}
+
+        # Check if the sentiment is present in the dictionary before updating
+        if result in country_distribution[country]:
+            country_distribution[country][result] += 1
+
+        # Calculate the average number of likes and retweets
+        average_likes = filtered_df['Likes'].mean()
+        average_retweets = filtered_df['Retweets'].mean()
+
+    return render_template('dashboard.html', hashtag=hashtag, sentiment_counts=sentiment_counts, country_distribution=country_distribution, countries=countries, average_likes=average_likes, average_retweets=average_retweets)
 
 
 if __name__ == '__main__':
